a64531aec0599ee942364e35030dd5f5f69f00cd
NikonovAO <nikonow@bk.ru>
2021-10-06 15:47:44 +0400

    fix: одинарные кавычки в HTML-разметке заменены на двойные

--- my-app-popover/src/HeaderBlock/HeaderBlock.js
+++ my-app-popover/src/HeaderBlock/HeaderBlock.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect} from "react";
+
 import Bell from "./Bell.svg";
 
-import './headerStyle.css';
+import "./headerStyle.css";
 
 export default function HeaderBlock() {
     const [count, setCount] = useState(0);
@@ -21,10 +22,10 @@ export default function HeaderBlock() {
     }, [count])
 
     return (
-        <header className='header'>
-            <h1 className='header__title'>Test task: Notification icon</h1>
-            <div className='header__notification-container'>
-                <button onClick={() => {}} className='header__notification-button'>
+        <header className="header">
+            <h1 className="header__title">Test task: Notification icon</h1>
+            <div className="header__notification-container">
+                <button onClick={() => {}} className="header__notification-button">
                     <img src={Bell} className="header__notification-container-logo" alt="logo-Bell" />
                     <span className="header__notification-container-counter"> {count} </span>
                     
--- my-app-popover/src/MainBlock/MainBlock.js
+++ my-app-popover/src/MainBlock/MainBlock.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import './mainStyle.css'
+import "./mainStyle.css";
 
 // TODO Поставить eslint в проект
 export default function MainBlock() {
@@ -12,18 +12,18 @@ export default function MainBlock() {
 
     return (
         <main className="main">
-            <h2 className='main__header'>Event management form</h2>
+            <h2 className="main__header">Event management form</h2>
 
-            <form name='formControlEvent' onSubmit={handleNewEventSubmit}>
+            <form name="formControlEvent" onSubmit={handleNewEventSubmit}>
                 <div className="main__form">
-                    <input type='text' name='nameEvent' placeholder='Введите название события...'/>
-                    <button type='submit' name='addEvent'>Отправить</button>
+                    <input type="text" name="nameEvent" placeholder="Введите название события..."/>
+                    <button type="submit" name="addEvent">Отправить</button>
                 </div>
             </form>
 
-            <button type='button' name='finishedAllEvent'>Пометить все события прочитанными</button>
-            <button type='button' name='deleteAllEvent'>Удалить все события</button>
-            <button type='button' name='togglePopup'>Скрыть / показать попап нотификаций (toggle btn) </button>
+            <button type="button" name="finishedAllEvent">Пометить все события прочитанными</button>
+            <button type="button" name="deleteAllEvent">Удалить все события</button>
+            <button type="button" name="togglePopup">Скрыть / показать попап нотификаций (toggle btn) </button>
         </main>
     )
 }
